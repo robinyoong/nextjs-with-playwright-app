d6895cd6f7c767145059c52a2337ad7ab07bc26b
robinyoong <robin.yoong@gmail.com>
2022-08-07 16:39:41 +0800

    add new feature

--- pages/index.js
+++ pages/index.js
@@ -14,7 +14,8 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!!!</a>
+          Welcome to{" "}
+          <a href="https://nextjs.org">Next.js with new features!!!</a>
         </h1>
 
         <p className={styles.description}>
