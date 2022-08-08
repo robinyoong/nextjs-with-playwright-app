ed01d549cbd3d26522a6c330ee1ed4ad0650589a
robinyoong <robin.yoong@gmail.com>
2022-08-08 23:19:28 +0800

    add image optimisation

---
+++ next.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  experimental: {
+    images: {
+      formats: ["image/webp"],
+      allowFutureImage: true,
+    },
+  },
+};
--- pages/index.js
+++ pages/index.js
@@ -14,10 +14,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to{" "}
-          <a href="https://nextjs.org">
-            Next.js with new features added with 3 tests
-          </a>
+          Welcome to <a href="https://nextjs.org">Next.js</a>
         </h1>
 
         <p className={styles.description}>
