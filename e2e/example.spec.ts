65a7be9dd83297750af3779f74924304d4665a3a
robinyoong <robin.yoong@gmail.com>
2022-08-08 18:26:47 +0800

    Revert test copy

--- e2e/example.spec.ts
+++ e2e/example.spec.ts
@@ -8,5 +8,5 @@ test("should navigate to the about page", async ({ page }) => {
   // The new url should be "/about" (baseURL is used there)
   await expect(page).toHaveURL("/about");
   // The new page should contain an h1 with "About Page"
-  await expect(page.locator("h1")).toContainText("About Page with Tests");
+  await expect(page.locator("h1")).toContainText("About Page");
 });
